77f14b77e0fdcbf338734c42cf541884fc19f2f3
Joshua <joshua.dadson@ibm.com>
2019-10-16 11:04:02 -0500

    Dropdown transition update

--- aipm-react/src/components/common-ui/Dropdown/Dropdown.js
+++ aipm-react/src/components/common-ui/Dropdown/Dropdown.js
@@ -10,8 +10,6 @@ class Dropdown extends React.Component {
 
         this.questions = this.props.questions
 
-        this.showDrop = this.showDrop.bind(this)
-        this.hideDrop = this.hideDrop.bind(this)
 
         this.state = {
             displayDrop: false,
@@ -24,38 +22,18 @@ class Dropdown extends React.Component {
 
     }
 
-    showDrop() {
-        // event.preventDefault()
-        this.setState({
-            displayDrop: true
-        }, 
-        // () => {document.addEventListener('click', this.hideDrop)}
-        )
-    }
-
-
-    hideDrop() {
-        this.setState({
-            displayDrop: false
-        }, 
-        // () => {document.removeEventListener('click', this.hideDrop)}
-        )
-    }
-
-
     
 
   render() {
       return (
         <div> 
-        <div className="icon" tabIndex="1" onClick={() => this.setState({
-            displayDrop: !this.state.displayDrop
+        <div className="icon" onClick={() => this.setState({
         })}>
             {this.state.lightningBlue ? <img src={lightningBoltBlue} onClick={() => this.setState({lightningBlue: !this.state.lightningBlue, width: "30px", opacity: "0", font: "1.6px"})}/> 
             : <img src={lightningBolt} onClick={() => this.setState({lightningBlue: !this.state.lightningBlue, width: "300px", opacity: "1", font: "16px"})}/>} 
             {/* {this.props.children }  */}
         </div>
-        <div className="dropdown-elements" style={{width: this.state.width, opacity: this.state.opacity, fontSize: this.state.font}}>{this.state.lightningBlue ? this.questions(): null}</div>
+        <div className="dropdown-elements" style={{width: this.state.width, opacity: this.state.opacity, fontSize: this.state.font}}>{this.questions()}</div>
              {/* {this.state.lightningBlue ? (this.setState({width: "300px"}), this.display="initial"): (this.state.width = "30px", this.display="none") } */}
     </div>    
       )
