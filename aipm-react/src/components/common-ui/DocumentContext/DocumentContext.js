77378880ef2544a6b9f8774810657ca3915b3b38
Joshua <joshua.dadson@ibm.com>
2019-10-22 16:31:51 -0500

    Adding Components Blockchain and Document Context

---
+++ aipm-react/src/components/common-ui/DocumentContext/DocumentContext.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './DocumentContext.css'
+
+class DocumentContext extends React.Component {
+
+    render() {
+        return (
+            <div className="DocumentContext">
+                Document Context
+                <div className="Content">
+                    Insert Content Here
+                </div>
+            </div>
+        )
+    }
+}
+
+export default DocumentContext;
