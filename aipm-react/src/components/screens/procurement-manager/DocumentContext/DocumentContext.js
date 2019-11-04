a20ab1d49557ed30c02a1b59be964bbc62db3944
Joshua <joshua.dadson@ibm.com>
2019-11-04 16:54:05 -0600

    Piechart styling

--- aipm-react/src/components/screens/procurement-manager/DocumentContext/DocumentContext.js
+++ aipm-react/src/components/screens/procurement-manager/DocumentContext/DocumentContext.js
@@ -1,32 +1,42 @@
 import {
-    LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, PieChart, Pie,
-  } from 'recharts';
+  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, PieChart, Pie, ResponsiveContainer, Cell,
+} from 'recharts';
 
-  import React from 'react'
-
-
-  const datas = [
-      { name: "Good", value: 2 },
-       { name: "Bad", value: 4 },
-       { name: "Moderate", value: 4 },
-    ];
-
-  const DocumentContext = (props) => {
-  
-        
+import React from 'react'
+import "./DocumentContext.css"
 
 
-    
-      var content = <img className="procurementImgs" src={require('../../../../assets/' + props.currStep + '.png')} />
-      if(props.currStep=="Inspect") {
-        content = (<PieChart width = {500} height = {500}>
-              <Pie data={datas} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
-            </PieChart>)
-        
-      } 
-      return content
-    }
+const datas = [
+  { name: "Good", value: 2 },
+  { name: "Bad", value: 4 },
+  { name: "Moderate", value: 4 },
+];
 
-  
+const DocumentContext = (props) => {
 
-  export default DocumentContext;
+
+
+
+
+  var content = <img className="procurementImgs" src={require('../../../../assets/' + props.currStep + '.png')} />
+  // if(props.currStep=="Inspect") {
+    const COLORS =  ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+  content = (
+    <ResponsiveContainer width="100%" height="100%" margin={{ top: 5, right: 5, bottom: 5, left: 5 }} >
+      <PieChart width={100} height={100} margin={{ top: 5, right: 5, bottom: 5, left: 5 }}>
+        <Pie data={datas} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius="90%" fill= "#8884d8" >
+        {
+            datas.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+          }
+        </Pie>
+      </PieChart>
+    </ResponsiveContainer>
+  )
+
+  // } 
+  return <div className="DocumentContext">{content}</div>
+}
+
+
+
+export default DocumentContext;
