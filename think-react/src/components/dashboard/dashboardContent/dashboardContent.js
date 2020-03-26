5865f224cc40e6449483bcf8b9e1ad2c6230f8b1
Joshua <joshua.dadson@ibm.com>
2020-03-26 16:41:49 -0500

    Took off robot health - fixing prod issues

--- think-react/src/components/dashboard/dashboardContent/dashboardContent.js
+++ think-react/src/components/dashboard/dashboardContent/dashboardContent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './dashboardContent.css';
 import Graph from '../../3DGraph/3DGraph';
-import RobotHealth from '../../RobotHealth/RobotHealth'
+// import RobotHealth from '../../RobotHealth/RobotHealth'
 
 class DashboardContent extends Component {
     render() {
@@ -10,7 +10,7 @@ class DashboardContent extends Component {
             // <p>what do u mean lol</p>
             <div className="contents-container">
                 <div className="r1-row robot-col card-padding robot-name">Robot 1</div>
-                <div className="r1-row robot-health card-padding card-color"><RobotHealth/></div>
+                {/* <div className="r1-row robot-health card-padding card-color"><RobotHealth/></div> */}
                 <div className="r1-row position card-padding card-color">
                     <Graph />
                 </div>
@@ -22,7 +22,7 @@ class DashboardContent extends Component {
                 <div className="j4-j5-j6-r1 j3-j6-col">u</div>
                 <div className="gap-line">line</div>
                 <div className="r2-row robot-col card-padding robot-name">Robot 2</div>
-                <div className="r2-row robot-health card-padding card-color"><RobotHealth/></div>
+                {/* <div className="r2-row robot-health card-padding card-color"><RobotHealth/></div> */}
                 <div className="r2-row position card-padding card-color"><Graph /></div>
                 <div className="j1-j2-j3-r2 j1-j4-col">s</div>
                 <div className="j1-j2-j3-r2 j2-j5-col">l</div>
