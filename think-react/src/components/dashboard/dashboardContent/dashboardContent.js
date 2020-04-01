36dc67a4dee805fb5990ba9c7ca9b149e7703bdd
Joshua <joshua.dadson@ibm.com>
2020-04-01 16:27:37 -0500

    mqtt

--- think-react/src/components/dashboard/dashboardContent/dashboardContent.js
+++ think-react/src/components/dashboard/dashboardContent/dashboardContent.js
@@ -12,23 +12,44 @@ import asset5 from '../../../Assets/Asset5.png';
 import asset6 from '../../../Assets/Asset6.png';
 import asset7 from '../../../Assets/Asset7.png';
 import asset8 from '../../../Assets/Asset8.png';
+import { Client } from 'paho-mqtt';
 
 
 class DashboardContent extends Component {
 
-    // state = {
-    //     sTorque: null,
-    //     lTorque: null,
-    //     uTorque: null,
-    //     rTorque: null,
-    //     bTorque: null,
-    //     tTorque: null,
-    //     xPos: [],
-    //     yPos: [],
-    //     zPos: [],
-    // }
+    state = {
+        sTorque: null,
+        lTorque: null,
+        uTorque: null,
+        rTorque: null,
+        bTorque: null,
+        tTorque: null,
+        xPos: [],
+        yPos: [],
+        zPos: [],
+        mqttClient: null
+    }
+
+    mqttCredentials = [
+        {
+            clientId: 'a:vrvzh6:' + Math.random().toString(16).substr(2, 8),
+            broker: "vrvzh6.messaging.internetofthings.ibmcloud.com",
+            subscribe: "iot-2/type/gsc-yaskawa-gw/id/gsc-yaskawa-01/evt/update/fmt/json",
+            username: "a-vrvzh6-lmttnkzxht",
+            password: "LRVitW+(soqXuZdJT!"
+        },
+        {
+            clientId: 'a:vrvzh6:' + Math.random().toString(16).substr(2, 8),
+            broker: "vrvzh6.messaging.internetofthings.ibmcloud.com",
+            subscribe: "iot-2/type/gsc-yaskawa-gw/id/gsc-yaskawa-01/evt/update/fmt/json",
+            username: "a-vrvzh6-lmttnkzxht",
+            password: "LRVitW+(soqXuZdJT!"
+        }
+    ]
 
     componentDidMount() {
+        this.mqttHandler('gsc-yaskawa-tire01');
+
         // var mqtt = require('mqtt')
         // var client = mqtt.connect('wss://test.mosquitto.org:8081')
 
@@ -41,26 +62,137 @@ class DashboardContent extends Component {
         // })
 
         // client.on('message', (topic, message) => {
-        //     var json = JSON.parse(message)
+            // var json = JSON.parse(message)
 
-        //     this.setState({
-        //         data: json,
-        //         healthScore: null,
-        //         sTorque: json.sTorque,
-        //         lTorque: json.lTorque,
-        //         uTorque: json.uTorque,
-        //         rTorque: json.rTorque,
-        //         bTorque: json.bTorque,
-        //         tTorque: json.tTorque,
-        //         xPos: [...this.state.xPos, json.xPos],
-        //         yPos: [...this.state.yPos, json.yPos],
-        //         zPos: [...this.state.zPos, json.zPos],
-        //     })
+            // this.setState({
+            //     data: json,
+            //     healthScore: null,
+            //     sTorque: json.sTorque,
+            //     lTorque: json.lTorque,
+            //     uTorque: json.uTorque,
+            //     rTorque: json.rTorque,
+            //     bTorque: json.bTorque,
+            //     tTorque: json.tTorque,
+            //     xPos: [...this.state.xPos, json.xPos],
+            //     yPos: [...this.state.yPos, json.yPos],
+            //     zPos: [...this.state.zPos, json.zPos],
+            // })
         //     console.log(json)
 
         // })
 
     }
+
+    mqttHandler = (device) => {
+        let mqtt_clientId = null;
+        let mqtt_broker = null;
+        let mqtt_username = null;
+        let mqtt_password = null;
+        switch (device) {
+            case 'gsc-yaskawa-tire01':
+                mqtt_clientId = this.mqttCredentials[0].clientId;
+                mqtt_broker = this.mqttCredentials[0].broker;
+                mqtt_username = this.mqttCredentials[0].username;
+                mqtt_password = this.mqttCredentials[0].password;
+                // debugger;
+                break;
+            case 'gsc-yaskawa-tire02':
+                mqtt_clientId = this.mqttCredentials[1].clientId;
+                mqtt_broker = this.mqttCredentials[1].broker;
+                mqtt_username = this.mqttCredentials[1].username;
+                mqtt_password = this.mqttCredentials[1].password;
+                // console.log("switch - device -" + device);
+                break;
+            // case 'default': 
+            // mqtt_clientId = this.mqttCredentials[0].clientId;
+            // mqtt_broker = this.mqttCredentials[0].broker;
+            // mqtt_username = this.mqttCredentials[0].username;
+            // mqtt_password = this.mqttCredentials[0].password;
+        }
+        // Create a client instance
+        let mqtt_client = new Client(mqtt_broker, 1883, mqtt_clientId);
+        // set callback handlers
+        mqtt_client.onConnectionLost = this.onConnectionLost;
+        mqtt_client.onMessageArrived = this.onMessageArrived;
+        this.setState({
+            mqttClient: mqtt_client
+        }, () => {
+            // this.client.connect({onSuccess:this.onConnect.bind(this)});
+            this.state.mqttClient.connect({
+                onSuccess: this.onConnect.bind(this),
+                onFailure: this.onFailure,
+                userName: mqtt_username,   // apikey
+                password: mqtt_password
+            })
+        })
+    }
+    // called when the client loses its connection
+    onConnectionLost = (responseObject) => {
+        if (responseObject.errorCode !== 0) {
+            console.log("onConnectionLost:" + responseObject.errorMessage);
+        }
+    }
+    onMessageArrived = (message) => {
+        console.log("inside onMessage 2");
+        this.onMessageArrivedCommon(message);
+    }
+    onConnect = (props) => {
+        // Once a connection has been made, make a subscription and send a message.
+        let subscribeString = null;
+        switch ('gsc-yaskawa-tire01') {
+            case 'gsc-yaskawa-tire01':
+                subscribeString = this.mqttCredentials[0].subscribe;
+                console.log("switch - subscribe -" + "gsc-yaskawa-tire01");
+                break;
+            case 'gsc-yaskawa-tire02':
+                subscribeString = this.mqttCredentials[0].subscribe;
+                console.log("switch - subscribe -" + "gsc-yaskawa-tire02");
+                break;
+        }
+        console.log("onConnect");
+        this.state.mqttClient.subscribe(subscribeString);
+    }
+    onFailure = (responseObject) => {
+        // Once a connection has been made, make a subscription and send a message.
+        console.log("onFailure" + JSON.stringify(responseObject));
+    }
+    // called when a message arrives
+    onMessageArrivedCommon = (message) => {
+        let myTopic = message.destinationName;
+        let parsedTopic = myTopic.split("/");
+        let deviceId = parsedTopic[4];
+        let valueCmdEvt = parsedTopic[6];
+        let textJson = parsedTopic[8];
+        if (textJson === "json") {
+            let json = JSON.parse(message.payloadString);
+            console.log("wholedata",valueCmdEvt,json);
+
+            this.setState({
+                data: json,
+                healthScore: null,
+                sTorque: json.sTorque,
+                lTorque: json.lTorque,
+                uTorque: json.uTorque,
+                rTorque: json.rTorque,
+                bTorque: json.bTorque,
+                tTorque: json.tTorque,
+                xPos: [...this.state.xPos, json.xPos],
+                yPos: [...this.state.yPos, json.yPos],
+                zPos: [...this.state.zPos, json.zPos],
+            })
+
+            // if (valueCmdEvt === "score") {
+            //     let score = [deviceId, iotPayload.speakingClassification, iotPayload.confidence, iotPayload.slot];
+            //     let cur_scoredata = this.state.scoredata;
+            //     cur_scoredata[iotPayload.slot - 1].score = score;
+            //     cur_scoredata[iotPayload.slot - 1].slot = iotPayload.slot;
+            //     this.setState({
+            //         scoredata: cur_scoredata
+            //     });
+            // }
+        }
+    }
+
 
     render() {
         return (
